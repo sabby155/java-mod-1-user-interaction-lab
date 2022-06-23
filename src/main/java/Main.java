4c46258a462d0d28a4a99cc8dc38131b0114fa41
sabby155 <nebula155@gmail.com>
2022-06-23 14:35:17 -0400

    User Interaction Lab complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         // your code here
+        System.out.println("Please enter your age.");
+        Scanner inputScanner = new Scanner(System.in);
+        int userInput = inputScanner.nextInt();
+        if(userInput < 18) {
+            System.out.println("Hello kiddos");
+            if (userInput < 10) {
+                System.out.println("Impressively young to be learning this!");
+            }
+            System.out.println("Good to see you here!");
+        }
     }
 }
