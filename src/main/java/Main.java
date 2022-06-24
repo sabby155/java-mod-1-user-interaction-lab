7f0efcb4649659f8ef7db9c77500a9fdedaf39d8
sabby155 <nebula155@gmail.com>
2022-06-24 12:33:07 -0400

    changes made in this branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,12 +6,19 @@ public class Main {
         System.out.println("Please enter your age.");
         Scanner inputScanner = new Scanner(System.in);
         int userInput = inputScanner.nextInt();
-        if(userInput < 18) {
+        if (userInput < 18) {
             System.out.println("Hello kiddos");
             if (userInput < 10) {
                 System.out.println("Impressively young to be learning this!");
             }
-            System.out.println("Good to see you here!");
+        } else if (userInput == 18) {
+            System.out.println("Hey there.");
+            System.out.println("We've been expecting you!");
+        } else if (userInput > 18) {
+            System.out.println("Hey there.");
+            System.out.println("Welcome to the program.");
         }
+        // print for all
+        System.out.println("Good to see you here!");
     }
 }
